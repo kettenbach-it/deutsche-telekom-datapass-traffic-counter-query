fe813b20801f0a29597bb26d4a6a74848dc96958
Volker Kettenbach <volker@kettenbach-it.de>
2019-05-02 10:11:33 +0200

    Initla checkin with finished program

---
+++ datapass.py
@@ -0,0 +1,17 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+url = "http://datapass.de/home?continue=true"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+data = soup.find("div", class_="barTextBelow").text
+billingPeriod = soup.find("td", class_="infoValue billingPeriod").text
+remainingTime = soup.find("td", class_="infoValue remainingTime").text
+
+print("Abrechnungsmonat: " + billingPeriod)
+print(data)
+print("Restzeit: " + remainingTime)
